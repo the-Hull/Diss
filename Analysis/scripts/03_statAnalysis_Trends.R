2688c07591fc8e8099ca7db71d3a0f5955674477
the-Hull <the-Hull@users.noreply.github.com>
2015-07-01 19:31:21 +0100

    colors, trend,
    and looking for things

--- Analysis/scripts/03_statAnalysis_Trends.R
+++ Analysis/scripts/03_statAnalysis_Trends.R
@@ -1,33 +1,103 @@
-
-# Fitting Linear Model to decomposed time series. -------------------------
-
+## First exploratory data analysis
 
 
-trendplot <- function(){
-      ## Maximizes the number of trophic groups in steady state by identifying
-      ## ideal cutoff
+#  Identify ideal window for cutting off data  ---------------------------------
 
-res <- numeric()
+ts_trendplotID(50,95)
 
-      for(i in 50:95){
-            trends <- ts_test(statsFull,cutoff=i*12, expno=1:7)
 
-            res[i-49] <- sum(trends$Trend, na.rm=T)
-            print(i)
+# Test if system dynamics are sensitive to summary stats ------------------
+
+
+
+## Creates new env to store all o
+plotdata <- new.env()
+
+fg <- unique(expdataFull$FGroup)
+expn <- unique(expdataFull$ExpNo)
+cells <- unique(expdataFull$CellCode)
+
+plotdata$Cell1 <- new.env()
+plotdata$Cell0 <- new.env()
+
+for(c in cells){
+      for(e in expn){
+            for(f in fg){
+
+
+                  # assign respective subsets to objects
+                  if(c=="Cell0"){
+                        assign(paste0("exp",e,".",f, ".", c),
+                               subset(expdataFull, FGroup==f & ExpNo==e & CellCode==c),
+                               plotdata$Cell0)
+                  } else {
+                        assign(paste0("exp",e,".",f, ".", c),
+                               subset(expdataFull, FGroup==f & ExpNo==e & CellCode==c),
+                               plotdata$Cell1)
+                  }
+
+
+            }
       }
-      x11()
-      plot(50:95,
-           res,
-           type="l",
-           lwd=1.2,
-           xlab="Time (years)",
-           ylab="Non-zero slopes (n)")
+}
 
-      abline(v=(which(res==min(res))+49),
-             lty=2,
-             col="grey70")
 
-      ideal <- which(res==min(res))+49
-      return(ts_test(statsFull,cutoff=ideal*12, expno=1:7))
+## Create graph space
+x11()
+layout(mat = matrix(c(1:4, rep(5,2)),
+                    nrow=2),
+       width=c(0.4,0.4,0.2)
+)
+## plot al experiments for each FGroup
 
+for(ce in cells){
+      for(f in 1:length(fg)){
+            names <- ls(pattern = fg[f], name = plotdata[[ce]])
+
+            for(n in 1:(length(names)-1)){
+
+                  dat <- get(names[n], envir  = plotdata[[ce]])
+
+
+                  stats <- summary_stats(dat)
+
+                  ts <- length(unique(dat$TimeStep))
+
+
+
+                  ## Plot if n == 1
+                  if(n==1){
+                        plot(stats$Median, ylim=c(min(stats$Min), max(stats$Max)))
+                        lines(1:nrow(stats), stats$Median, col=1, lwd=1.5)
+                  } else {
+
+                        lines(1:nrow(stats), stats$Median, col=1, lwd=1.5)
+
+                  }
+
+
+                  if(f==length(fg) & n==(length(names)-1)){
+                              plot(1:1, type='n', axes=F, xaxt='n', yaxt='n', xlab="", ylab="")
+
+                  }
+
+                  for(t in 1:ts){
+                        ts.dat <- subset(dat, TimeStep==t)
+
+                        points(ts.dat$MassDens,
+                               col=add_alpha(n+1, alpha=0.2),
+                               pch=16,
+                               cex=0.5)
+
+                  }
+            }
+
+
+
+
+
+            rm(dat)
+            rm(stats)
+
+      }
 }
---
+++ Analysis/source/ts_trend_IDbest.R
@@ -0,0 +1,33 @@
+
+# Fitting Linear Model to decomposed time series. -------------------------
+
+
+
+ts_trendplotID <- function(lower, upper){
+      ## Maximizes the number of trophic groups in steady state by identifying
+      ## ideal cutoff
+
+res <- numeric()
+
+      for(i in lower:upper){
+            trends <- ts_test(statsFull,cutoff=i*12, expno=1:7)
+
+            res[i-(lower-1)] <- sum(trends$Trend, na.rm=T)
+            print(i)
+      }
+      x11()
+      plot(lower:upper,
+           res,
+           type="o",
+           lwd=1.2,
+           xlab="Time (years)",
+           ylab="Non-zero slopes (n)")
+
+      abline(v=(which(res==min(res))+(lower-1)),
+             lty=2,
+             col="grey70")
+
+      ideal <- which(res==min(res))+(lower-1)
+      return(ts_test(statsFull,cutoff=ideal*12, expno=1:7))
+
+}
